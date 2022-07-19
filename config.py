61696734b95880e513b686e2d0cee994b0e026fc
daniel-gallagher <daniel.gallagher1@ucdconnect.ie>
2022-07-19 11:12:42 +0100

    minor changes

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+SQLALCHEMY_DATABASE_URI = r"postgres://xxvdxwsyomftak:dc13179505f8305eb243a500610710814c748cb7e055428e82cd69045dde8ada@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d60qi5pk47am21"
+SECRET_KEY = os.getenv("SECRET_KEY", "defaultkey97(j3bg##-y-f)6fza7vpd(rh8q5n$&tt@w5n$r8**40m*&*uw")
