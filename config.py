802f72fb4c6be937671c0c497edfd81cf3a319b1
daniel-gallagher <daniel.gallagher1@ucdconnect.ie>
2022-07-19 15:53:55 +0100

    make for release

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
-SQLALCHEMY_DATABASE_URI = r"postgresql://xxvdxwsyomftak:dc13179505f8305eb243a500610710814c748cb7e055428e82cd69045dde8ada@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d60qi5pk47am21"
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "development_key")
+SQlALCHEMY_TRACK_MODIFICATIONS = False
