83c4c6519dec3f16ffaa1f447ce3644abb639a0f
daniel-gallagher <daniel.gallagher1@ucdconnect.ie>
2022-07-19 16:02:39 +0100

    minor changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import re
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URI = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 SECRET_KEY = os.getenv("SECRET_KEY", "development_key")
 SQlALCHEMY_TRACK_MODIFICATIONS = False
